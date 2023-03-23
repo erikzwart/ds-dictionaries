d99ad9d0ac293edd940e000df4ce0e488596537d
erikzwart <e.zwart@umcg.nl>
2023-03-23 10:59:02 +0100

    yaml to tibble dsUpload input

--- R/compare-dsUpload-excel-to-yaml.R
+++ R/compare-dsUpload-excel-to-yaml.R
@@ -38,4 +38,38 @@ filepath <- excel_file
 vars <- read_xlsx(path = filepath, sheet = 1) %>% as_tibble()
 
 dict <- yaml::yaml.load_file(yaml_file)
-dict <- as_tibble(dict$Variables)
+
+yaml_variable <- yaml::yaml.load(dict$Variables)
+
+library(purrr)
+vars2 <- purrr::map_dfr(yaml_variable, bind_rows)
+
+all.equal(vars, vars2)
+
+# catagories
+cats <- read_xlsx(path = excel_file, sheet = 2) %>% as_tibble()
+cats <- cats %>%
+  rename(value = name, name = variable) %>%
+  mutate(
+    name = as.character(name),label = as.character(label))
+vars <- nest_join(vars, cats, by = "name")
+
+vars <- vars %>% bind_rows()
+
+yaml_categories <- yaml::yaml.load(dict$Categories)
+
+cats2 <- purrr::map_dfr(yaml_categories, bind_rows)
+
+cats2 <- cats2 %>%
+  rename(value = name, name = variable) %>%
+  mutate(
+    name = as.character(name),
+    value = as.numeric(value),
+    isMissing = as.logical(isMissing),
+    label = as.character(label)
+  )
+vars2 <- nest_join(vars2, cats2, by = "name")
+
+vars2 <- vars2 %>% bind_rows()
+
+all.equal(vars, vars2)
